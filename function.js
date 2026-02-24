fa0ff28c1e70b8ff59e1c5d44018d4d2a429259a
Reyad-Hoq <rafiulhoq40@gmail.com>
2026-02-24 23:03:54 +0600

    button interactivated

--- function.js
+++ function.js
@@ -5,3 +5,16 @@ function updateCount () {
   rejectedCount.innerText = rejectedList.length;
 }
 updateCount ();
+
+function toggleStyle (id) {
+  const interviewing = document.getElementById("interview-btn");
+  const rejecting = document.getElementById("reject-btn");
+  const allbtn = document.getElementById("all-btn");
+  const selected = document.getElementById(id);
+// first removing button class
+  allbtn.classList.remove("bg-[#3B82F6]", "text-white");
+  interviewing.classList.remove("bg-white","bg-[#3B82F6]","text-white");
+  rejecting.classList.remove("bg-white","bg-[#3B82F6]","text-white");
+// then adding button class to clicked button 
+  selected.classList.add("bg-[#3B82F6]", "text-white");
+}
